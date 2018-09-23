15b7af3104d54b0a8900f831331afaf4b93b3e91
richbothe <richbothe@gmail.com>
2018-09-23 17:27:33 -0500

    Updates.

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,17 @@
-$(document).on('click','.navbar-collapse.in',function(e) {
-    if( $(e.target).is('a') ) {
-        $(this).collapse('hide');
-    }
-});
+$(document).ready(function() {
+    $('nav.navbar').on('click','.navbar-collapse.in',function(e) {
+        if( $(e.target).is('a') ) {
+            $(this).collapse('hide');
+        }
+    });
 
-$(".nav a").on("click", function(){
-   $(".nav").find(".active").removeClass("active");
-   $(this).parent().addClass("active");
+    $(".nav a").on("click", function(){
+       $(".nav").find(".active").removeClass("active");
+       $(this).parent().addClass("active");
+    });
+
+    $('#myTabs a').click(function (e) {
+  e.preventDefault()
+  $(this).tab('show')
+})
 });
